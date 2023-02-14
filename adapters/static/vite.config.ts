3d99a0d9fc61b1a3ea9b463af7468d798a71a761
Matthieu Riegler <kyro38@gmail.com>
2023-02-14 23:37:12 +0100

    re-generate static adapter

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -1,4 +1,4 @@
-import { staticAdaptor } from '@builder.io/qwik-city/adaptors/static/vite';
+import { staticAdapter } from '@builder.io/qwik-city/adapters/static/vite';
 import { extendConfig, qwikCity } from '@builder.io/qwik-city/vite';
 import baseConfig from '../../vite.config';
 
@@ -11,9 +11,9 @@ export default extendConfig(baseConfig, () => {
       },
     },
     plugins: [
-      qwikCity({ basePathname: '/qwik-app/' }),
-      staticAdaptor({
-        origin: 'https://jeanmeche.github.io/qwik-app/',
+      qwikCity({ basePathname: '/qwik-app' }),
+      staticAdapter({
+        origin: 'https://jeanmeche.github.io',
       }),
     ],
   };
